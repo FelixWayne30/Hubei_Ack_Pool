07fac9fc788cee9f3afb9db324ae11fbde72c64e
weiyuanbiao <1764372494@qq.com>
2025-04-09 20:25:47 +0800

    新建springboot工程

---
+++ src/main/java/com/example/hubeiatlasbackend/HubeiAtlasBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.hubeiatlasbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HubeiAtlasBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HubeiAtlasBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hubeiatlasbackend/HubeiAtlasBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hubeiatlasbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HubeiAtlasBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
